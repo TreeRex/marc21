e2217165e19cf8dd59bd93995151152f30873ec4
Tom Emerson <tremerson@gmail.com>
2014-01-04 23:35:23 +0000

    Add field extraction with concomitant unit tests.

--- marc21.go
+++ marc21.go
@@ -6,11 +6,13 @@ package marc21
 import (
 	"errors"
 	"io"
+	"strings"
 )
 
 var (
 	errInvalidLength      = errors.New("marc21: record length is invalid")
 	errNoRecordTerminator = errors.New("marc21: record must end in a RT")
+	errInvalidLeader      = errors.New("marc21: leader is invalid")
 )
 
 const (
@@ -23,6 +25,121 @@ const (
 	leaderSize    = 24
 	maxRecordSize = 99999
 )
+
+type Entry struct {
+	fieldLength    int
+	startingOffset int
+}
+
+type VariableField struct {
+	rawData [][]byte
+}
+
+type MarcRecord struct {
+	RawRecord         []byte
+	Status            byte
+	Type              byte
+	BibLevel          byte
+	CharacterEncoding byte
+	EncodingLevel     byte
+	CatalogingForm    byte
+	MultipartLevel    byte
+	Directory         map[string][]Entry
+}
+
+// Array of valid values for positions in the MARC 21 leader
+var marc21LeaderValues = []struct {
+	offset int
+	values string
+}{
+	{5, "acdnp"},
+	{6, "acdefgijkmoprt"},
+	{7, "abcdims"},
+	{8, " a"},
+	{9, " a"},
+	{10, "2"},
+	{11, "2"},
+	// 12 -- 16 Base address of data
+	{17, " 1234578uz"},
+	{18, " aciu"},
+	{19, " abc"},
+	{20, "4"},
+	{21, "5"},
+	{22, "0"},
+	{23, "0"},
+}
+
+func NewMarcRecord(rawData []byte) (*MarcRecord, error) {
+	// this assumes that rawData is a superficially valid Z39.2
+	// record: the length is encoded in the first five bytes and
+	// the final byte is a recordTerminator.
+	if !validLeader(rawData) {
+		return nil, errInvalidLeader
+	}
+
+	m := new(MarcRecord)
+
+	m.RawRecord = rawData
+	m.Status = rawData[5]
+	m.Type = rawData[6]
+	m.BibLevel = rawData[7]
+	m.CharacterEncoding = rawData[8]
+	m.EncodingLevel = rawData[17]
+	m.CatalogingForm = rawData[18]
+	m.MultipartLevel = rawData[19]
+
+	m.Directory = decodeDirectory(rawData)
+
+	return m, nil
+}
+
+func (m *MarcRecord) GetRawField(tag string) VariableField {
+	entry := m.Directory[tag]
+	if entry == nil {
+		return VariableField{}
+	}
+
+	result := make([][]byte, len(entry))
+	for i := range entry {
+		start := entry[i].startingOffset;
+		end := entry[i].startingOffset + entry[i].fieldLength
+		result[i] = m.RawRecord[start:end]
+	}
+
+	return VariableField{result}
+}
+
+func (f *VariableField) ValueCount() int {
+	return len(f.rawData)
+}
+
+func (f *VariableField) GetRawValue(i int) []byte {
+	return f.rawData[i]
+}
+
+func validLeader(leader []byte) bool {
+	for i := range marc21LeaderValues {
+		s := string(leader[marc21LeaderValues[i].offset])
+		if strings.IndexAny(marc21LeaderValues[i].values, s) == -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func decodeDirectory(record []byte) map[string][]Entry {
+	baseAddress := decodeDecimal(record[12:17])
+
+	m := make(map[string][]Entry)
+
+	for i := 24; record[i] != fieldTerminator; i += 12 {
+		tag := string(record[i : i+3])
+		m[tag] = append(m[tag],
+			Entry{decodeDecimal(record[i+3 : i+7]), baseAddress + decodeDecimal(record[i+7:i+12])})
+	}
+
+	return m
+}
 
 func readRecord(r io.Reader) (int, []byte, error) {
 	tmp := make([]byte, 5)
--- marc21_test.go
+++ marc21_test.go
@@ -4,26 +4,29 @@ package marc21
 
 import (
 	"bytes"
+//	"fmt"
 	"strings"
 	"testing"
 )
 
 const (
-	recordData   = "00034 the string needs to be long" + string(recordTerminator)
-	recordLength = len(recordData)
+	fullRecord = "00458nam a22001577u 4500001001200000005001700012008004100029035001600070245005400086260004100140300003500181650003100216710003300247988001300280906000700293\x1e000000002-7\x1e20120831093346.0\x1e821202|1937    |||||||  |||| |0||||eng|d\x1e0 \x1faocm83544809\x1e00\x1faGarden exhibition /\x1fcSan Francisco Museum of Art.\x1e0 \x1faSan Francisco :\x1fbThe Museum,\x1fc[1937]\x1e  \x1fa1 folded sheet (4p.) ;\x1fc14 cm.\x1e 0\x1faHorticultural exhibitions.\x1e2 \x1faSan Francisco Museum of Art.\x1e  \x1fa20020608\x1e  \x1f0MH\x1e\x1d"
+	fullRecordLen = len(fullRecord)
+
+	titleStatement = "00\x1faGarden exhibition /\x1fcSan Francisco Museum of Art.\x1e";
 )
 
 func TestReadRecord(t *testing.T) {
-	d := strings.NewReader(recordData)
+	d := strings.NewReader(fullRecord)
 
 	n, rec, e := readRecord(d)
 	if e != nil {
 		t.Fatalf("Unable to read record: %v", e)
 	}
-	if n != recordLength {
-		t.Errorf("Returned record size should be %d, got %v", recordLength, n)
+	if n != fullRecordLen {
+		t.Errorf("Returned record size should be %d, got %v", fullRecordLen, n)
 	}
-	if !bytes.Equal([]byte(recordData), rec) {
+	if !bytes.Equal([]byte(fullRecord), rec) {
 		t.Errorf("Read data does not equal source data: %v", rec)
 	}
 }
@@ -37,3 +40,37 @@ func TestDecodeDecimal(t *testing.T) {
 		t.Errorf("Conversion of \"0\" did not equal 0, rather %v", v)
 	}
 }
+
+func TestLeaderValidation(t *testing.T) {
+	if !validLeader([]byte(fullRecord)) {
+		t.Errorf("Valid leader did not pass validation")
+	}
+}
+
+func TestDirectoryLoader(t *testing.T) {
+	d := decodeDirectory([]byte(fullRecord))
+
+	if len(d) != 11 {
+		t.Errorf("Invalid entry count in directory")
+	}
+}
+
+func TestRawFieldExtraction(t *testing.T) {
+	m,_ := NewMarcRecord([]byte(fullRecord))
+	
+	field := m.GetRawField("245")
+	if field.ValueCount() != 1 {
+		t.Fatalf("More than one entry value returned")
+	}
+	
+	if !bytes.Equal([]byte(titleStatement), field.GetRawValue(0)) {
+		t.Errorf("Returned entry does not match raw data")
+	}
+
+	// ask for a non-existent field
+	field = m.GetRawField("666")
+	if field.ValueCount() != 0 {
+		t.Errorf("Non-existent field returns non-0 value count")
+	}
+}
+
